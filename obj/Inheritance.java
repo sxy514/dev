672bafd326a5b8ef1568834fff974064bc635039
zhangxueyou <65749067+sxy514@users.noreply.github.com>
2022-12-14 17:14:37 +0000

    modified:   .vscode/launch.json 	new file:   obj/Inheritance.java 	modified:   obj/PackageAPI.java 	modified:   "obj/\350\257\264\346\230\216"

---
+++ obj/Inheritance.java
@@ -0,0 +1,19 @@
+package obj;
+
+// 类继承 , 父类将属性和方法继承给子类 
+public class Inheritance {
+    protected int x = 5; // protected 关键字 表示 受保护的
+
+    public void inherMethod() {
+        System.out.println("父类继承给子类的方法");
+    }
+}
+
+// 子类 使用 extends 关键字 继承父类，就可以访问父类中的属性和方法啦
+class SubClass extends Inheritance {
+    public static void main(String[] args) {
+        SubClass myObj = new SubClass();
+        System.out.println(myObj.x);
+        myObj.inherMethod();
+    }
+}
--- obj/PackageAPI.java
+++ obj/PackageAPI.java
@@ -5,10 +5,11 @@ import java.util.Scanner; // 导入单个类 Scanner 用于获取用户输入
 // 导入后就可以使用 Scanner 类了
 public class PackageAPI {
     public static void main(String[] args) {
-        // 创建一个 Scanner 类的对象
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter username");
-        String userName = myObj.nextLine(); // 调用对象的方法 获取用户输入后 将其赋值给字符串变量 userName
-        System.out.println("Username is: " + userName);
+        try (// 创建一个 Scanner 类的对象
+                Scanner myObj = new Scanner(System.in)) {
+            System.out.println("Enter username");
+            String userName = myObj.nextLine(); // 调用对象的方法 获取用户输入后 将其赋值给字符串变量 userName
+            System.out.println("Username is: " + userName);
+        }
     }
 }
