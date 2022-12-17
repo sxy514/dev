d12546c5cc20648bcf3afcca9de5f9dc09e9908a
zhangxueyou <65749067+sxy514@users.noreply.github.com>
2022-12-17 15:14:50 +0000

    modified:   .vscode/launch.json 	new file:   obj/obj/Main.java 	new file:   obj/obj/RegularExpression.java 	modified:   obj/obj/TryCatch1.java 	modified:   "obj/\350\257\264\346\230\216"

---
+++ obj/obj/RegularExpression.java
@@ -0,0 +1,23 @@
+package obj;
+
+// 导入 Pattern类 - 定义模式（在搜索中使用）
+import java.util.regex.Matcher;
+// 导入 Matcher类 - 用于搜索 模式
+import java.util.regex.Pattern;
+
+// 本示例展示 正则表达式 的用法  
+public class RegularExpression {
+
+    public static void main(String[] args) {
+
+        // 注意用法
+        Pattern pattern = Pattern.compile("sixieyou", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("my name is sixieyou");
+        boolean matchFound = matcher.find();
+        if (matchFound) {
+            System.out.println("Match found");
+        } else {
+            System.out.println("Match not found");
+        }
+    }
+}
--- obj/obj/TryCatch1.java
+++ obj/obj/TryCatch1.java
@@ -2,7 +2,20 @@ package obj;
 
 // 本示例 将 展示 try_catch 的使用 ，找到会抛异常的代码 try{} 起来
 public class TryCatch1 {
+
+    // 抛出关键字，throw 关键字允许你创建自定义错误，与异常类型一起使用，
+    static void checkAge(int age) {
+        if (age < 18) {
+            // 当 age 小于 18 时，抛出自定义的异常
+            throw new ArithmeticException("拒绝访问-您必须年满18岁.");
+        } else {
+            System.out.println("允许访问-你够大了!");
+        }
+    }
+
     public static void main(String[] args) {
+
+        checkAge(19); // Set age to 15
 
         // 将你觉得会抛异常的代码 try{} 起来
         try {
@@ -15,6 +28,7 @@ public class TryCatch1 {
             // finally 语句允许你在 try_catch 结束后执行代码
             System.out.println(" 'try catch' 完成");
         }
+
     }
 
 }
